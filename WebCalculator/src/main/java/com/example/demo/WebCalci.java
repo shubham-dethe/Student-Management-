558c22170f77b3730a600687e9200ba31c496f2c
shubham-dethe <shubhamdethe88@gmail.com>
2025-07-09 18:27:47 +0530

    Uploaded Calculator Project

---
+++ WebCalculator/src/main/java/com/example/demo/WebCalci.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebCalci {
+	
+	
+	@GetMapping("add/{a}/{b}")
+	int add(@PathVariable("a") int a, @PathVariable("b") int b)
+	{
+		return a+b;
+	}
+	
+	@GetMapping("sub/{a}/{b}")
+	int sub(@PathVariable("a") int a, @PathVariable("b") int b)
+	{
+		return a-b;
+	}
+	
+	
+	@GetMapping("mul/{a}/{b}")
+	int mul(@PathVariable("a") int a, @PathVariable("b") int b)
+	{
+		return a*b;
+	}
+	
+	
+	@GetMapping("div/{a}/{b}")
+	int div(@PathVariable("a") int a, @PathVariable("b") int b)	{
+		return a/b;
+	}
+	
+	
+	
+	
+
+}
